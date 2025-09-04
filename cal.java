8791da9f3f369639e7512f5207cb2ad72d82ca19
omprakash700222-hash <omprakash700222@gmail.com>
2025-09-04 17:52:01 +0530

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,15 @@
+//import java.util.*;
+//public class First {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        if (num1 == num2) {
+//            System.out.println("num1=num2");
+//        } else if (num1 > num2) {
+//            System.out.println("num1>num2");
+//        } else if (num1 < num2) {
+//            System.out.println("num1<num2");
+//        }
+//    }
+//}
